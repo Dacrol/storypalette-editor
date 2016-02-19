f90352cecb0abf6eabae13598e7a7778b1488a89
Rava Fonseca <ravafonseca@outlook.com>
2016-02-19 16:20:08 +0100

    added pagination for resources closes #10

--- src/app/edit/resourceListCtrl.js
+++ src/app/edit/resourceListCtrl.js
@@ -1,16 +1,64 @@
 angular.module('sp.editor.edit.resourceListCtrl', [
-  'sp.editor.common.access', 'dndLists'
+  'sp.editor.common.access', 'dndLists', 'ui.bootstrap'
 ])
 
-.controller('ResourceListCtrl', function($scope, $state, access, audioPlayer, palettes, resources, dialog, $modal) {
+.controller('ResourceListCtrl', function($scope, $state, orderByFilter, filterFilter, access, audioPlayer, palettes, resources, dialog, $modal) {
   $scope.isCollapsed = true;
-
+  
+  function load()
+  {
   resources.all().then(function(data) {
     // TODO Move filtering serverside
-    $scope.resources = access.filterRestrictedData(data);
-  });
+    $scope.resources = $scope.restrictedResources = orderByFilter(access.filterRestrictedData(data), 'name');
+    $scope.itemsPerPage = 10;
+    $scope.currentPage = 1;
+    $scope.maxSize = 5;
+    
+    $scope.filteredResources = $scope.resources.slice(0, $scope.itemsPerPage);  
 
-  console.log(angular.version);
+    $scope.totalItems = $scope.resources.length;
+
+    $scope.filterResources = function(type) {
+        $scope.typeFilter = type;
+        $scope.resources = search($scope.restrictedResources, $scope.typeFilter, $scope.query);
+        $scope.currentPage = 1;
+        $scope.filteredResources = makePage($scope.resources); 
+    };
+
+    $scope.$watch('query', function (term) {
+        $scope.resources = search($scope.restrictedResources, $scope.typeFilter, term);
+        $scope.currentPage = 1;
+        $scope.filteredResources = makePage($scope.resources); 
+    });
+        
+    $scope.$watch('currentPage', function() { 
+        $scope.filteredResources = makePage($scope.resources);
+    });    
+  });
+  }
+  
+  load(); 
+  
+  function makePage(list) {
+      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+      var end = begin + $scope.itemsPerPage;
+      return list.slice(begin, end);
+  }
+
+  function search(list, typeFilter, query) {
+      var filteredList = list;
+
+      if (typeFilter) {
+          var obj = { type: typeFilter };
+
+          filteredList = filterFilter(list, obj);
+      }
+
+      if (query) {
+          filteredList = filterFilter(filteredList, query);
+      }
+      return filteredList;
+  }
 
   $scope.iconClasses = {
     sound: 'icon-music',
@@ -19,11 +67,7 @@ angular.module('sp.editor.edit.resourceListCtrl', [
   };
   
   $scope.typeFilter = '';
-
-  $scope.filterResources = function(type) {
-    $scope.typeFilter = type;
-  };
-
+  
   var dialogOptions = {
     templateUrl: 'edit/addEditResource.tpl.html',
     controller: 'AddEditResourceCtrl',
@@ -44,26 +88,14 @@ angular.module('sp.editor.edit.resourceListCtrl', [
     dialogOptions.resolve = {resource: function() {return resource;}};
     openDialog();
   };
-         $scope.models = {
-        selected: null,
-        lists: {"A": [], "B": []}
-    };
-
-    // Generate initial model
-    for (var i = 1; i <= 3; ++i) {
-        $scope.models.lists.A.push({name: "Item A" + i});
-        $scope.models.lists.B.push({name: "Item B" + i});
-    }
-
-    // Model to JSON for demo purpose
-    $scope.$watch('models', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
-    }, true);
   
   var openDialog = function () {
     console.log(dialog);
     dialog.dialog(dialogOptions, function() {
       console.log('reloading page');
+
+      $scope.filteredResources = [];
+      load();
       $state.go($state.current, {}, {reload: true});
     });
   };
